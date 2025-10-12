2726c8592cec430350d93ea7180bcd62ef1ba13d
SamShah <sieam240@gmail.com>
2025-10-13 05:52:45 +0600

    task-3

--- Task3/manual_wheel_control.ino
+++ Task3/manual_wheel_control.ino
@@ -1,46 +1,80 @@
 #define NUM_CHANNELS 8
-#define SERIAL_BUFFER_SIZE 64
 
 int channels[NUM_CHANNELS];
-char serialBuffer[SERIAL_BUFFER_SIZE];
-int bufferIndex = 0;
-bool insidePacket = false;
+bool newDataAvailable = false;
+String serialBuffer = "";
+bool readingData = false;
 
-void readChannel() {
-  while (Serial1.available() > 0) {
-    char incomingChar = Serial1.read();
-
-    if (incomingChar == '<') {
-      bufferIndex = 0;
-      insidePacket = true;
-      continue;
+void setup() {
+    Serial.begin(115200);
+    
+    for (int i = 0; i < NUM_CHANNELS; i++) {
+        channels[i] = 1500;
     }
-
-    if (insidePacket) {
-      if (incomingChar == '>') {
-        serialBuffer[bufferIndex] = '\0';
-        parsePacket(serialBuffer);
-        insidePacket = false;
-        bufferIndex = 0;
-      }
-      else if (bufferIndex < SERIAL_BUFFER_SIZE - 1) {
-        serialBuffer[bufferIndex] = incomingChar;
-        bufferIndex++;
-      } else {
-        insidePacket = false;
-        bufferIndex = 0;
-      }
-    }
-  }
 }
 
-void parsePacket(char* packetString) {
-  char* token = strtok(packetString, " ");
-  int chanIndex = 0;
+void loop() {
+    readChannel();
+    
+    if (newDataAvailable) {
+        Serial.print("Channels: ");
+        for (int i = 0; i < NUM_CHANNELS; i++) {
+            Serial.print(channels[i]);
+            if (i < NUM_CHANNELS - 1) {
+                Serial.print(" ");
+            }
+        }
+        Serial.println();
+        newDataAvailable = false;
+    }
+    
+    delay(10);
+}
 
-  while (token != NULL && chanIndex < NUM_CHANNELS) {
-    channels[chanIndex] = atoi(token);
-    chanIndex++;
-    token = strtok(NULL, " ");
-  }
+void readChannel() {
+    while (Serial1.available() > 0) {
+        char incomingChar = Serial1.read();
+        
+        if (incomingChar == '<') {
+            serialBuffer = "";
+            readingData = true;
+        }
+        else if (incomingChar == '>' && readingData) {
+            processChannelData(serialBuffer);
+            serialBuffer = "";
+            readingData = false;
+        }
+        else if (readingData) {
+            serialBuffer += incomingChar;
+        }
+    }
+}
+
+void processChannelData(String data) {
+    int tempChannels[NUM_CHANNELS];
+    int channelIndex = 0;
+    
+    int startIndex = 0;
+    int endIndex = data.indexOf(' ');
+    
+    while (endIndex != -1 && channelIndex < NUM_CHANNELS) {
+        String valueStr = data.substring(startIndex, endIndex);
+        tempChannels[channelIndex] = valueStr.toInt();
+        channelIndex++;
+        startIndex = endIndex + 1;
+        endIndex = data.indexOf(' ', startIndex);
+    }
+    
+    if (channelIndex < NUM_CHANNELS && startIndex < data.length()) {
+        String valueStr = data.substring(startIndex);
+        tempChannels[channelIndex] = valueStr.toInt();
+        channelIndex++;
+    }
+    
+    if (channelIndex == NUM_CHANNELS) {
+        for (int i = 0; i < NUM_CHANNELS; i++) {
+            channels[i] = tempChannels[i];
+        }
+        newDataAvailable = true;
+    }
 }
